23d19c0811980d09c5d42e771c6ff0bda8f75ca5
Shokin Ilya <atlus3@mail.ru>
2021-09-11 18:53:29 +0300

    add: new tests

---
+++ Source/UnrealTesting/Tests/SandboxTests.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tests/SandboxTests.h"
+#include "CoreMinimal.h" // минимальная зависимость
+#include "Misc/AutomationTest.h" // подключаем для запуска тестов
+
+// добавление простого класса для тестирования
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FMathMaxInt, // имя класса
+    "UnrealTesting.Math.MaxInt", // pretty name он нужен для вложености в session frontend
+    // флаги, приоритет категория тестирования и т.д.
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FMathMaxSqrt,
+    "UnrealTesting.Math.Sqrt",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+// функция для запуска теста
+// не важно что возвращает функция, главное что выводится в лог, если есть что то в логе то тест провален
+// но если возвращать false то тест провалится
+bool FMathMaxInt::RunTest(const FString& Parameters)
+{
+    // функция для добавление информации для теста
+    AddInfo("Max [int] func testing");
+
+    // если нужно добавить цвет теста желтым добавляем
+    // AddWarning("add warning");
+
+    // тестируем что либо, добавлям описание и проверяем на правдивость
+    TestTrue("2 different positive numbers", FMath::Max(13, 25) == 25);
+    // тестируем что либо но проверяем на значения
+    TestEqual("2 equal positive numbers", FMath::Max(25, 25), 25);
+    // макрос на тесттру
+    TestTrueExpr(FMath::Max(0, 123) == 123);
+    TestTrue("2 zeroes", FMath::Max(0, 0) == 0);
+    TestTrue("Negative number and zero", FMath::Max(-23323, 0) == 0);
+    TestTrue("Negative number and positive number", FMath::Max(-23323, 1) == 1);
+    TestTrue("2 negative numbers", FMath::Max(-45, -87) == -45);
+    TestEqual("2 equal negative numbers", FMath::Max(-7, -7), -7);
+    
+    return true;
+}
+
+bool FMathMaxSqrt::RunTest(const FString& Parameters)
+{
+    AddInfo("Sqrt function testing");
+
+    TestEqual("Sqrt(4) [0]", FMath::Sqrt(4.0f), 2.0f);
+    // TestEqual("Sqrt(3) [1]", FMath::Sqrt(3.0f), 1.7f);
+    // ниже идет указание точности для вещественных чисел
+    TestEqual("Sqrt(3) [2]", FMath::Sqrt(3.0f), 1.7f, 0.1f);
+    TestEqual("Sqrt(3) [3]", FMath::Sqrt(3.0f), 1.73f, 0.01f);
+    TestEqual("Sqrt(3) [4]", FMath::Sqrt(3.0f), 1.73205f, 1.e-5f);
+    
+    return true;
+}
--- Source/UnrealTesting/UnrealTesting.Build.cs
+++ Source/UnrealTesting/UnrealTesting.Build.cs
@@ -8,9 +8,14 @@ public class UnrealTesting : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.Add("UnrealTesting");
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
