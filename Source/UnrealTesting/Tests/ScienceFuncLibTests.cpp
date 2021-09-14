346a9bdff6de67d16886f2be8977f0b5ecf4df29
Shokin Ilya <atlus3@mail.ru>
2021-09-14 20:48:22 +0300

    add: stress test and test has error

--- Source/UnrealTesting/Tests/ScienceFuncLibTests.cpp
+++ Source/UnrealTesting/Tests/ScienceFuncLibTests.cpp
@@ -12,6 +12,16 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(
     "UnrealTesting.Science.Fibonacci.Simple",
     EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
 
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FFibonacciStress,
+    "UnrealTesting.Science.Fibonacci.Stress",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::StressFilter | EAutomationTestFlags::LowPriority);
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FFibonacciLogHasErrors,
+    "UnrealTesting.Science.Fibonacci.LogHasErrors",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
 bool FFibonacciSimple::RunTest(const FString& Parameters)
 {
 
@@ -46,6 +56,39 @@ bool FFibonacciSimple::RunTest(const FString& Parameters)
         const FString InfoString = FString::Printf(TEXT("test value: %i, expected value: %i"), Data.TestValue, Data.ExpectedValue);
         TestEqual(InfoString, UScienceFuncLib::Fibonacci(Data.TestValue), Data.ExpectedValue);
     }
+    
+    return true;
+}
+
+bool FFibonacciStress::RunTest(const FString& Parameters)
+{
+    AddInfo("Fibonacci stress testing");
+
+    // for(int32 i = 2; i < 40; ++i)
+    // {
+    //     TestTrueExpr(UScienceFuncLib::Fibonacci(i) == UScienceFuncLib::Fibonacci(i-1) + UScienceFuncLib::Fibonacci(i-2));
+    // }
+
+    int32 PrevPrevValue = 0;
+    int32 PrevValue = 1;
+    for(int32 i = 2; i < 40; ++i)
+    {
+        const int32 CurrentValue = UScienceFuncLib::Fibonacci(i);
+        TestTrueExpr(CurrentValue == PrevValue + PrevPrevValue);
+
+        PrevPrevValue = PrevValue;
+        PrevValue = CurrentValue;
+    }
+    
+    return true;
+}
+
+bool FFibonacciLogHasErrors::RunTest(const FString& Parameters)
+{
+    AddInfo("Fibonacci negative number on input produces error");
+
+    AddExpectedError("Invalid input for Fibonacci", EAutomationExpectedErrorFlags::Contains);
+    UScienceFuncLib::Fibonacci(-11);
     
     return true;
 }
